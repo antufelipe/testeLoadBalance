b55a9cc3bf6e79490fb917ef553e1cb8cebbf2c3
Felipe antunes pedro <fe.lipeantunes@hotmail.com>
2020-03-04 16:28:46 -0300

    initial

---
+++ Aplicacao-De-Teste/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Aplicacao_De_Teste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WeatherForecast Get()
+        {
+            return new WeatherForecast()
+            {
+                Date = DateTime.Now,
+                Summary = "Maquina 1",
+                TemperatureC = 10
+            };
+        }
+    }
+}
